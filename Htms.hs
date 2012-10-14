f1878f72b952b5c47595f1800e17eb028b178bfc
Dan Keller <dbk40@case.edu>
2012-10-13 22:22:52 -0400

    added socket code

--- Htms.hs
+++ Htms.hs
@@ -1,11 +1,36 @@
---
--- Copyright (c) 2006 Don Stewart - http://www.cse.unsw.edu.au/~dons/
--- GPL version 2 or later (see http://www.gnu.org/copyleft/gpl.html)
---
 import System.Environment
- 
--- | 'main' runs the main program
-main :: IO ()
-main = getArgs >>= print . haqify . head
+import Network hiding (accept)
+import Network.Socket hiding (send, sendTo, recv, recvFrom)
+import Network.Socket.ByteString
+import Data.ByteString.Char8 as B
+import Control.Concurrent
+import Control.Monad
 
-haqify s = "Haq! " ++ s
+torquePort = 28002
+
+main = withSocketsDo $ do
+    addr <- getAddrInfo torquePort
+    sock <- socket AF_INET Datagram defaultProtocol
+    bindSocket sock (addrAddress addr)
+    process sock
+    where process sock = do
+        (msg, _, sender) <- recvFrom sock 1024
+        putStrLn msg
+        process sock
+
+{- 
+main = withSocketsDo $
+    (listenOn $ PortNumber 6669) >>= accLoop
+
+accLoop sock = do
+    (conn, _) <- accept sock
+    forkIO $ reply conn
+    accLoop sock
+
+reply conn = do
+    str <- recv conn 4096
+    print str
+    if not $ B.null str
+        then reply conn
+        else return ()
+-}
