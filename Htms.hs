1eab18148c75a82c08954caa84c0277a85688381
Daniel Keller <dbk40@case.edu>
2012-10-19 21:23:06 +0000

    can track state

--- Htms.hs
+++ Htms.hs
@@ -1,12 +1,37 @@
 import qualified Data.Map as Map
+import Data.DateTime
+import Network.Socket
 import Util
+import Control.Concurrent
+import Control.Applicative
+import Control.Monad.Trans.State.Strict
 
 torquePort = 28002
 
-actHeartbeat :: MsgHandler
-actHeartbeat sender (header, msg) = do
-    putStrLn $ "badump: " ++ show sender
+--message types
+t_listreq = 6
+t_listresp = 8
+t_inforeq = 10
+t_inforesp = 12
+t_heartbeat = 22
 
-main = mainLoop $ Map.fromList
-    [(32, actHeartbeat)] --actually 22
+data Game = Game { lastSeen :: DateTime }
+
+type Games = Map.Map SockAddr Game
+
+actHeartbeat :: MsgHandler Integer
+actHeartbeat sender msg mvar = do
+    --return $ putStrLn $ show num
+    --newGame <- Game getCurrentTime
+    --Map.insert sender newGame games
+    print $ body msg
+    putMVar mvar printinc
+    return ()
+    where
+        printinc n = do
+            print n
+            return (n + 1)
+
+main = mainLoop 0 $ Map.fromList
+    [(32, actHeartbeat)]
 
--- Util.hs
+++ Util.hs
@@ -2,6 +2,7 @@ module Util (
     Header,
     msgType, flags, seqNum,
     Message,
+    header, body,
     MsgHandler,
     mainLoop
 ) where
@@ -17,6 +18,7 @@ import Control.Monad
 import qualified Data.Map as Map
 import Data.Int
 import qualified Data.Binary as Bin
+import Control.Monad.Trans.State.Strict
 
 torquePort = 28002
 
@@ -36,19 +38,42 @@ data Message = Message { header :: Header, body :: B.ByteString }
 
 toMessage msg = Message (takeHeader msg) (takeMessage msg)
 
-type MsgHandler = SockAddr -> Message -> IO ()
+--parameters are origin address, message, and a message box for a state modifier function
+type MsgHandler s = SockAddr -> Message -> MVar (s -> IO s) -> IO ()
 
-mainLoop :: Map.Map Int8 MsgHandler -> IO ()
-mainLoop actions = withSocketsDo $ do
+--recvAllFrom = sequence . repeat $ recvFrom sock 1024
+--or performAll = sequence . repeat
+
+mainLoop :: s -> Map.Map Int8 (MsgHandler s) -> IO ()
+mainLoop initState actions = withSocketsDo $ do
+
+    --create the socket
     sock <- socket AF_INET Datagram defaultProtocol
     setSocketOption sock ReuseAddr 1
     bindSocket sock (SockAddrInet torquePort iNADDR_ANY)
-    process sock
+
+    --start tracking state
+    stateChanger <- newEmptyMVar
+    forkIO $ trackState stateChanger initState
+
+    --start recieving data
+    process stateChanger sock
+
     where
-        process sock = do
+        trackState mvar state = do
+            changer <- takeMVar mvar
+            changer state >>= trackState mvar
+
+        process mvar sock = do
             (bytes, sender) <- recvFrom sock 1024
             let msg = toMessage bytes
             case Map.lookup (msgType $ header msg) actions of
-                Just handler -> (forkIO $ handler sender msg) >> return ()
-                Nothing      -> putStrLn $ "got unknown message #" ++ show (msgType $ header msg)
-            process sock
+
+                Just handler -> do
+                        handler sender msg mvar
+                        process mvar sock
+                        return () --not reached
+
+                Nothing      -> do
+                        putStrLn $ "got unknown message #" ++ show (msgType $ header msg)
+                        process mvar sock
